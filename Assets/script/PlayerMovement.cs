0523063543742b9ba31e8047e3ac333dc8c7475c
Raymond Cheung <cheungtingyiu@gmail.com>
2018-06-09 15:34:34 +0800

    first commit

---
+++ Assets/script/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// We mark this as "Fixed" Update because we
+    // are using it to mess with physics.
+	void FixedUpdate () 
+    {
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if( Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime,0 ,0, ForceMode.VelocityChange);
+            //Only executed if condiditon is met
+        }
+
+        if (rb.position.y < - 1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+	}
+}
